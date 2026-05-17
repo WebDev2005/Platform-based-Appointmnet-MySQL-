7437643e7c4ae7f78f2c5ecf7d9070e49119e9c3
Brian kent Balladares <162076774+WebDev2005@users.noreply.github.com>
2026-05-17 18:00:03 +0800

    Update database connection details in db.php

--- db.php
+++ db.php
@@ -1,9 +1,17 @@
 <?php
-function getDB() {
-    $conn = new mysqli("sql100.infinityfree.com", "if0_41611772", "bkballadares23", "if0_41611772_appointment");
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+function getDB() {
+
+    $host = "mysql.railway.internal";
+    $user = "root";
+    $pass = "XJsuxLJoBNiLvYWZfPXdKmjjmFXANJQx";
+    $db   = "railway";
+    $port = "3306";
+
+    $conn = mysqli_connect($host, $user, $pass, $db, $port);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
 
     return $conn;
